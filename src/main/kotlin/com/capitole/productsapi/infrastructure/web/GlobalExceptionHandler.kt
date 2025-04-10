4acfa16a1db31c5699260e7dc3553064f71f4263
FanyCastro <estefania.castro.vizoso@gmail.com>
2025-04-10 12:02:08 +0200

    feat: logs in json format

--- src/main/kotlin/com/capitole/productsapi/infrastructure/web/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/capitole/productsapi/infrastructure/web/GlobalExceptionHandler.kt
@@ -2,9 +2,8 @@ package com.capitole.productsapi.infrastructure.web
 
 import com.capitole.productsapi.infrastructure.web.dto.ErrorResponse
 import jakarta.validation.ConstraintViolationException
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -12,13 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 import java.util.stream.Collectors
 
 @RestControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler () {
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handleValidationExceptions(
         ex: MethodArgumentNotValidException
     ): ErrorResponse {
+        logger.error("Invalid parameters MethodArgumentNotValidException - ${ex.bindingResult.allErrors}")
         val errors = ex.bindingResult.fieldErrors.stream()
             .map { error ->
                 "${error.field}: ${error.defaultMessage ?: "validation error"}"
@@ -37,6 +38,7 @@ class GlobalExceptionHandler {
     fun handleConstraintViolationExceptions(
         ex: ConstraintViolationException
     ): ErrorResponse {
+        logger.error("Invalid parameters ConstraintViolationException - ${ex.constraintViolations}")
         val errors = ex.constraintViolations.map { violation ->
             "${violation.propertyPath}: ${violation.message}"
         }
@@ -46,5 +48,16 @@ class GlobalExceptionHandler {
                 message = "Validation failed",
                 errors = errors
             )
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleGenericError(ex: Exception): ErrorResponse {
+        logger.error("Something went wrong - ${ex.javaClass} ${ex.message}")
+        return ErrorResponse(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            message = "Internal server error",
+            errors = listOf("SERVER_ERROR")
+        )
     }
 }
